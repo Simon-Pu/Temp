25df75ca8a6c7a9d095b82c4a54cb867feb3d07d
SimonPu <SimonPu@via.com.tw>
2019-06-17 19:14:15 +0800

    1. Updated of cuda version to 10.1.168-1 2. Note about install cuDNN steps.

--- install_cuda_docker.sh
+++ install_cuda_docker.sh
@@ -2,12 +2,21 @@
 # http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/
 # Install CUDA
 # wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
+# 
+cuda-repo=cuda-repo-ubuntu1604_10.1.168-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${cuda-repo}
+sudo dpkg -i ${cuda-repo}
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt upgrade -y
 sudo apt install cuda -y
+
+#Install CuDNN
+#https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/
+#libcudnn7-dev_7.6.0.64-1+cuda9.2_amd64.deb
+#wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.0.64-1+cuda9.2_amd64.deb
+#libcudnn7-dev_7.6.0.64-1+cuda10.1_amd64.deb
+#wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.0.64-1+cuda10.1_amd64.deb
 
 # Install docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
