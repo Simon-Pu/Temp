6be9e0cf1c31649d1a47eea454a4a185ae30b194
SimonPu <SimonPu@via.com.tw>
2019-09-12 17:44:04 +0800

    1. add of installinig prebuilt NV DALI packages 2. add of pip upgrade

--- environment.sh
+++ environment.sh
@@ -4,4 +4,10 @@ apt-get install -y libxrender1
 apt-get install -y libxext-dev
 apt-get install -y unzip
 apt-get install -y nano
+pip install --upgrade pip
 pip install opencv-python
+
+#Installing prebuilt DALI packages
+pip install --extra-index-url https://developer.download.nvidia.com/compute/redist/weekly/cuda/10.0 nvidia-dali-weekly
+pip install --extra-index-url https://developer.download.nvidia.com/compute/redist/weekly/cuda/10.0 nvidia-dali-tf-plugin-weekly
+#pip install -r requirements.txt
