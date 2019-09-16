d306f00e290026cbc31f887e2628e1cf241cf695
SimonPu <SimonPu@via.com.tw>
2019-09-16 16:20:49 +0800

    Add Detectron of package dependencies

--- environment.sh
+++ environment.sh
@@ -7,6 +7,18 @@ apt-get install -y nano
 pip install --upgrade pip
 pip install opencv-python
 
+# install Detectron dependencies
+pip install setuptools
+pip install Cython
+pip install matplotlib
+pip install numpy
+pip install pyyaml
+pip install mock
+pip install scipy
+pip install six
+pip install future
+pip install protobuf
+
 #Installing prebuilt DALI packages
 pip install --extra-index-url https://developer.download.nvidia.com/compute/redist/weekly/cuda/10.0 nvidia-dali-weekly
 pip install --extra-index-url https://developer.download.nvidia.com/compute/redist/weekly/cuda/10.0 nvidia-dali-tf-plugin-weekly
